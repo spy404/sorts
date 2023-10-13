2f2e4bfe0841768011291cbf3d9d846d7cee7b05
Taha Daneshmand <spy404.work@gmail.com>
2023-10-13 18:08:06 +0000

    Exchange sort - Python

---
+++ exchange_sort.py
@@ -0,0 +1,13 @@
+def exchange_sort(numbers: list[int]) -> list[int]:
+    numbers_length = len(numbers)
+    for i in range(numbers_length):
+        for j in range(i + 1, numbers_length):
+            if numbers[j] < numbers[i]:
+                numbers[i], numbers[j] = numbers[j], numbers[i]
+    return numbers
+
+
+if __name__ == "__main__":
+    user_input = input("Enter numbers separated by a comma:\n").strip()
+    unsorted = [int(item) for item in user_input.split(",")]
+    print(exchange_sort(unsorted))
