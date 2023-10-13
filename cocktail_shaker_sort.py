9c4fb8d62b06ef1adbfbf20410abc06bd551ebe1
Taha Daneshmand <spy404.work@gmail.com>
2023-10-13 17:57:24 +0000

    Cocktail shaker sort - Python

---
+++ cocktail_shaker_sort.py
@@ -0,0 +1,26 @@
+def cocktail_shaker_sort(unsorted: list) -> list:
+    for i in range(len(unsorted) - 1, 0, -1):
+        swapped = False
+
+        for j in range(i, 0, -1):
+            if unsorted[j] < unsorted[j - 1]:
+                unsorted[j], unsorted[j - 1] = unsorted[j - 1], unsorted[j]
+                swapped = True
+
+        for j in range(i):
+            if unsorted[j] > unsorted[j + 1]:
+                unsorted[j], unsorted[j + 1] = unsorted[j + 1], unsorted[j]
+                swapped = True
+
+        if not swapped:
+            break
+    return unsorted
+
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
+    user_input = input("Enter numbers separated by a comma:\n").strip()
+    unsorted = [int(item) for item in user_input.split(",")]
+    print(f"{cocktail_shaker_sort(unsorted) = }")
