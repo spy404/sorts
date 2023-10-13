d6b3390b695b0aab1419f3447e944d5711ed4964
Taha Daneshmand <spy404.work@gmail.com>
2023-10-13 17:54:52 +0000

    Circle sort - Python

---
+++ circle_sort.py
@@ -0,0 +1,50 @@
+def circle_sort(collection: list) -> list:
+    if len(collection) < 2:
+        return collection
+
+    def circle_sort_util(collection: list, low: int, high: int) -> bool:
+        swapped = False
+
+        if low == high:
+            return swapped
+
+        left = low
+        right = high
+
+        while left < right:
+            if collection[left] > collection[right]:
+                collection[left], collection[right] = (
+                    collection[right],
+                    collection[left],
+                )
+                swapped = True
+
+            left += 1
+            right -= 1
+
+        if left == right and collection[left] > collection[right + 1]:
+            collection[left], collection[right + 1] = (
+                collection[right + 1],
+                collection[left],
+            )
+
+            swapped = True
+
+        mid = low + int((high - low) / 2)
+        left_swap = circle_sort_util(collection, low, mid)
+        right_swap = circle_sort_util(collection, mid + 1, high)
+
+        return swapped or left_swap or right_swap
+
+    is_not_sorted = True
+
+    while is_not_sorted is True:
+        is_not_sorted = circle_sort_util(collection, 0, len(collection) - 1)
+
+    return collection
+
+
+if __name__ == "__main__":
+    user_input = input("Enter numbers separated by a comma:\n").strip()
+    unsorted = [int(item) for item in user_input.split(",")]
+    print(circle_sort(unsorted))
