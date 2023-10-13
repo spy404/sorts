c82efd8aae7a6e478dbba373709df8c6c2dec965
Taha Daneshmand <spy404.work@gmail.com>
2023-10-13 17:27:39 +0000

    Bogo sort - Python

---
+++ bogo_sort.py
@@ -0,0 +1,38 @@
+from __future__ import annotations
+
+
+def comp_and_swap(array: list[int], index1: int, index2: int, direction: int) -> None:
+  if (direction == 1 and array[index1] > array[index2]) or (
+        direction == 0 and array[index1] < array[index2]
+    ):
+        array[index1], array[index2] = array[index2], array[index1]
+
+
+def bitonic_merge(array: list[int], low: int, length: int, direction: int) -> None:
+    if length > 1:
+        middle = int(length / 2)
+        for i in range(low, low + middle):
+            comp_and_swap(array, i, i + middle, direction)
+        bitonic_merge(array, low, middle, direction)
+        bitonic_merge(array, low + middle, middle, direction)
+
+
+def bitonic_sort(array: list[int], low: int, length: int, direction: int) -> None:
+    if length > 1:
+        middle = int(length / 2)
+        bitonic_sort(array, low, middle, 1)
+        bitonic_sort(array, low + middle, middle, 0)
+        bitonic_merge(array, low, length, direction)
+
+
+if __name__ == "__main__":
+    user_input = input("Enter numbers separated by a comma:\n").strip()
+    unsorted = [int(item.strip()) for item in user_input.split(",")]
+
+    bitonic_sort(unsorted, 0, len(unsorted), 1)
+    print("\nSorted array in ascending order is: ", end="")
+    print(*unsorted, sep=", ")
+
+    bitonic_merge(unsorted, 0, len(unsorted), 0)
+    print("Sorted array in descending order is: ", end="")
+    print(*unsorted, sep=", ")
