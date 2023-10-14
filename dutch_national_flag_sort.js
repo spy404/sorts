273deced2dca5bea85dc857edfa48b5fb7f9c257
Taha Daneshmand <spy404.work@gmail.com>
2023-10-14 16:15:00 +0000

    Dutch national flag sort - JavaScript

---
+++ dutch_national_flag_sort.js
@@ -0,0 +1,24 @@
+export function dutchNationalFlagSort(nums) {
+  let low = 0
+  let mid = 0
+  let high = nums.length - 1
+
+  while (mid <= high) {
+    switch (nums[mid]) {
+      case 0:
+        ;[nums[low], nums[mid]] = [nums[mid], nums[low]]
+        low++
+        mid++
+        break
+      case 1:
+        mid++
+        break
+      case 2:
+        ;[nums[mid], nums[high]] = [nums[high], nums[mid]]
+        high--
+        break
+    }
+  }
+
+  return nums
+}
