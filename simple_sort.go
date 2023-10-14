e7b68b87f022f308a2e11451f18098ff0840cc9e
Taha Daneshmand <spy404.work@gmail.com>
2023-10-14 17:20:09 +0000

    Simple sort - Go

---
+++ simple_sort.go
@@ -0,0 +1,25 @@
+package sort
+
+import "github.com/TheAlgorithms/Go/constraints"
+
+func Simple[T constraints.Ordered](arr []T) []T {
+	for i := 0; i < len(arr); i++ {
+		for j := 0; j < len(arr); j++ {
+			if arr[i] < arr[j] {
+				arr[i], arr[j] = arr[j], arr[i]
+			}
+		}
+	}
+	return arr
+}
+
+func ImprovedSimple[T constraints.Ordered](arr []T) []T {
+	for i := 1; i < len(arr); i++ {
+		for j := 0; j < len(arr)-1; j++ {
+			if arr[i] < arr[j] {
+				arr[i], arr[j] = arr[j], arr[i]
+			}
+		}
+	}
+	return arr
+}
