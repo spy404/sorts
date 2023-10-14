83fe88c16ac6e5607724349de98eac67808b18ab
Taha Daneshmand <spy404.work@gmail.com>
2023-10-14 17:33:32 +0000

    Selection sort - PHP

---
+++ selection_sort.php
@@ -0,0 +1,24 @@
+<?php
+
+function selectionSort(array $array)
+{
+    $length = count($array);
+
+    for ($i = 0; $i < $length; $i++) {
+        $lowest = $i;
+
+        for ($j = $i + 1; $j < $length; $j++) {
+            if ($array[$j] < $array[$lowest]) {
+                $lowest = $j;
+            }
+        }
+
+        if ($i !== $lowest) {
+            $temp = $array[$i];
+            $array[$i] = $array[$lowest];
+            $array[$lowest] = $temp;
+        }
+    }
+
+    return $array;
+}
